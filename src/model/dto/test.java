5c635bdfa7bea89b4c26f890d402c7e5c8bee1d5
doucebaskouame <smile-dev2@smiledev2-pc>
2013-12-17 08:10:16 +0000

    ajout de test

---
+++ src/model/dto/test.java
@@ -0,0 +1,13 @@
+package model.dto;
+
+import javax.faces.event.SystemEvent;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.print("Hello world");
+	}
+
+}
